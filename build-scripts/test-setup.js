d6befa46a59e954edcd6fb27be31a013bd7251bc
Michael Reilly <1reillym@gmail.com>
2017-05-08 22:11:22 -0500

    initial commit

---
+++ build-scripts/test-setup.js
@@ -0,0 +1,5 @@
+// not transpiled, so must use CommonJS and ES5
+// Register babel to transpile before our tests run.
+require('babel-register')();
+// disable webpack features that Mocha does not understand.
+require.extensions['.css'] = function() {};
